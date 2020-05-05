9f6d5ccd802508e3e4b73e2d5869562f78e096cf
IrfanMonir <irfanuddin211@gmail.com>
2020-05-05 13:16:25 +0600

    Implemented api for notifications as read

--- GigHub/Models/UserNotification.cs
+++ GigHub/Models/UserNotification.cs
@@ -19,7 +19,7 @@ namespace GigHub.Models
 
         public ApplicationUser User { get; private set; }
         public Notification Notification { get; private set; }
-        public bool IsRead { get; set; }
+        public bool IsRead { get;private set; }
         protected UserNotification()
         {
              
@@ -33,6 +33,10 @@ namespace GigHub.Models
             
             User = user;
             Notification = notification;
+        }
+        public void Read()
+        {
+            IsRead = true;
         }
     }
 }
