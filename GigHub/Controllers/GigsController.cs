df1f90a75c597d3eebdf13dc9b93639989f6524c
IrfanMonir <irfanuddin211@gmail.com>
2020-05-07 23:21:30 +0600

    Implemented gig details

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -55,6 +55,28 @@ namespace GigHub.Controllers
             _context.SaveChanges();
             return RedirectToAction("MyUpcomingGigs", "Gigs");
         }
+        public ActionResult Details(int id)
+        {
+            var gig = _context.Gigs
+                .Include(g => g.Artist)
+                .Include(g => g.Genre)
+                .SingleOrDefault(g => g.Id == id);
+            if (gig == null)
+                return HttpNotFound();
+            var viewModel = new GigDetailsViewModel
+            {
+                Gig =gig
+            };
+            if (User.Identity.IsAuthenticated)
+            {
+                var userId = User.Identity.GetUserId();
+                viewModel.IsAttending = _context.Attendences.Any(a => a.GigId == gig.Id && a.AttendeeId == userId);
+                viewModel.IsFollowong = _context.Followings.Any(f => f.FolloweeId == gig.ArtistId && f.FollowerId == userId);
+            }
+            
+            
+            return View("Details",viewModel);
+        }
         [Authorize]
         public ActionResult Attending()
         {
