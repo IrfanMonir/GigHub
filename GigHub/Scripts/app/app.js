b469f5f1cd1f0451f8f91c38917ebdaf8adecbae
IrfanMonir <irfanuddin211@gmail.com>
2020-05-13 10:08:31 +0600

    Refactoring using revealing module

---
+++ GigHub/Scripts/app/app.js
@@ -0,0 +1,39 @@
+﻿var GigsController = function () {
+    var init = function () {
+        $(".js-toggle-attendence").click(function (e) {
+            var button = $(e.target);
+            if (button.hasClass("btn-default")) {
+                $.post("/api/attendences", { gigId: button.attr("data-gig-id") })
+                    .done(function () {
+                        button
+                            .removeClass("btn-default")
+                            .addClass("btn-info")
+                            .text("Going");
+
+                    })
+                    .fail(function () {
+                        alert("You are already going.");
+                    });
+            }
+            else {
+                $.ajax({
+                    url: "/api/attendences/" + button.attr("data-gig-id"),
+                    method: "DELETE"
+                })
+                    .done(function () {
+                        button
+                            .removeClass("btn-info")
+                            .addClass("btn-default")
+                            .text("Going?");
+                    })
+                    .fail(function () {
+                        alert("Something failed");
+                    });
+            }
+
+        });
+    };
+    return {
+        init: init
+    }
+}();
