711f388b580f32004ffb00bcc71742b7f77c954e
IrfanMonir <irfanuddin211@gmail.com>
2020-05-13 10:43:41 +0600

    Applying the dry principle

--- GigHub/Scripts/app/app.js
+++ GigHub/Scripts/app/app.js
@@ -1,38 +1,31 @@
 ﻿var GigsController = function () {
+    var button;
     var init = function () {
         $(".js-toggle-attendence").click(toggleAttendence);       
     };
 
     var toggleAttendence = function (e) {
-        var button = $(e.target);
+        button = $(e.target);
         if (button.hasClass("btn-default")) {
             $.post("/api/attendences", { gigId: button.attr("data-gig-id") })
-                .done(function () {
-                    button
-                        .removeClass("btn-default")
-                        .addClass("btn-info")
-                        .text("Going");
-
-                })
-                .fail(function () {
-                    alert("You are already going.");
-                });
+                .done(done)
+                .fail(fail);
         }
         else {
             $.ajax({
                 url: "/api/attendences/" + button.attr("data-gig-id"),
                 method: "DELETE"
             })
-                .done(function () {
-                    button
-                        .removeClass("btn-info")
-                        .addClass("btn-default")
-                        .text("Going?");
-                })
-                .fail(function () {
-                    alert("Something failed");
-                });
+                .done(done)
+                .fail(fail);
         }
+    };
+    var done = function () {
+        var text = (button.text() == "Going") ? "Going?" : "Going";
+        button.toggleClass("btn-info").toggleClass("btn-default").text(text);
+    };
+    var fail = function () {
+        alert("Something failed");
     };
     return {
         init: init
