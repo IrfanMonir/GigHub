700807d72789202bb2c643365a83c2d45ecd1913
IrfanMonir <irfanuddin211@gmail.com>
2020-05-13 10:21:07 +0600

    Cleaning the init method

--- GigHub/Scripts/app/app.js
+++ GigHub/Scripts/app/app.js
@@ -1,37 +1,38 @@
 ﻿var GigsController = function () {
     var init = function () {
-        $(".js-toggle-attendence").click(function (e) {
-            var button = $(e.target);
-            if (button.hasClass("btn-default")) {
-                $.post("/api/attendences", { gigId: button.attr("data-gig-id") })
-                    .done(function () {
-                        button
-                            .removeClass("btn-default")
-                            .addClass("btn-info")
-                            .text("Going");
+        $(".js-toggle-attendence").click(toggleAttendence);       
+    };
 
-                    })
-                    .fail(function () {
-                        alert("You are already going.");
-                    });
-            }
-            else {
-                $.ajax({
-                    url: "/api/attendences/" + button.attr("data-gig-id"),
-                    method: "DELETE"
+    var toggleAttendence = function (e) {
+        var button = $(e.target);
+        if (button.hasClass("btn-default")) {
+            $.post("/api/attendences", { gigId: button.attr("data-gig-id") })
+                .done(function () {
+                    button
+                        .removeClass("btn-default")
+                        .addClass("btn-info")
+                        .text("Going");
+
                 })
-                    .done(function () {
-                        button
-                            .removeClass("btn-info")
-                            .addClass("btn-default")
-                            .text("Going?");
-                    })
-                    .fail(function () {
-                        alert("Something failed");
-                    });
-            }
-
-        });
+                .fail(function () {
+                    alert("You are already going.");
+                });
+        }
+        else {
+            $.ajax({
+                url: "/api/attendences/" + button.attr("data-gig-id"),
+                method: "DELETE"
+            })
+                .done(function () {
+                    button
+                        .removeClass("btn-info")
+                        .addClass("btn-default")
+                        .text("Going?");
+                })
+                .fail(function () {
+                    alert("Something failed");
+                });
+        }
     };
     return {
         init: init
